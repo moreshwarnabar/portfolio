cc516bd466c31e5e5ebad10183221d040cb78d7a
moreshwarnabar <mrnabar@gmail.com>
2024-03-31 21:20:37 -0700

    completed the projects page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -71,7 +71,7 @@ const Contact = () => {
   };
 
   return (
-    <section className="relative flex lg:flex-row flex-col max-container overflow-y-auto">
+    <section className="relative flex lg:flex-row flex-col max-container h-[100vh]">
       {alert.show && <Alert {...alert} />}
       <div className="flex-1 min-2-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,73 @@
 import React from 'react';
 
+import { projects } from '../constants';
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
+import CTA from '../components/CTA';
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        My{' '}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Projects
+        </span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          I've participated on multiple projects. Many of them are available in
+          my GitHub profile. Feel free to explore the codebase and contribute
+          your ideas for further enhancements. Your collaboration is highly
+          valued!
+        </p>
+      </div>
+
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map(proj => (
+          <div className="lg:w-[400px] w-full" key={proj.name}>
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${proj.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={proj.iconUrl}
+                  alt="Project Icon"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
+                {proj.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{proj.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
+                <Link
+                  to={proj.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt="arrow"
+                  className="w-4 h-4 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
+    </section>
+  );
 };
 
 export default Projects;
